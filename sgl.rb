efe1f02b948bb883ea13ce0c77950c46cf9d1560
Koichiro Eto <koichiro.eto@gmail.com>
2020-08-14 23:02:31 +0900

    Init.

---
+++ sgl.rb
@@ -0,0 +1,20 @@
+# Copyright (c) 2020, Koichiro Eto, All rights reserved.
+# See LICENSE
+
+module SGL
+  DEFAULT_TYPE = "cocoa"
+  #DEFAULT_TYPE = "opengl"
+end
+
+unless defined?($sgl_type) && $sgl_type
+  $sgl_type = SGL::DEFAULT_TYPE
+end
+
+case $sgl_type
+when "cocoa"
+  require "sgl/cocoa"
+#when "opengl"
+#  require "sgl/opengl"
+#else
+  raise "unknown type"
+end
---
+++ sgl/cocoa.rb
@@ -0,0 +1,8 @@
+# Copyright (c) 2004-2020, Koichiro Eto, All rights reserved.
+# See LICENSE
+
+require "sgl/cocoa-app"
+require "sgl/cocoa-functions"
+
+include SGL
+$__a__ = Application.new
