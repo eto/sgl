9323da852c1021c48333eac7bed2de6f39b2286d
Koichiro Eto <koichiro.eto@gmail.com>
2021-12-20 04:14:19 +0100

    Update.

---
+++ prepare/README.md
@@ -0,0 +1,7 @@
+This library requires "Yet another OpenGL wrapper for Ruby"
+https://github.com/vaiorabbit/ruby-opengl
+
+1. Use gem to install the "Yet another OpenGL wrapper for Ruby"
+$ gem install opengl-bindings
+
+
---
+++ prepare/prepare1.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+require 'opengl'
+require 'glfw'
+
+OpenGL.load_lib()
+GLFW.load_lib()
+
+include OpenGL
+include GLFW
+
+# Press ESC to exit.
+key_callback = GLFW::create_callback(:GLFWkeyfun) do |window_handle, key, scancode, action, mods|
+  if key == GLFW_KEY_ESCAPE && action == GLFW_PRESS
+    glfwSetWindowShouldClose(window_handle, 1)
+  end
+end
+
+if __FILE__ == $0
+  glfwInit()
+
+  window = glfwCreateWindow( 640, 480, "Simple example", nil, nil )
+  glfwMakeContextCurrent( window )
+  glfwSetKeyCallback( window, key_callback )
+
+  while glfwWindowShouldClose( window ) == 0
+    width_ptr = ' ' * 8
+    height_ptr = ' ' * 8
+    glfwGetFramebufferSize(window, width_ptr, height_ptr)
+    width = width_ptr.unpack('L')[0]
+    height = height_ptr.unpack('L')[0]
+    ratio = width.to_f / height.to_f
+
+    glViewport(0, 0, width, height)
+    glClear(GL_COLOR_BUFFER_BIT)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-ratio, ratio, -1.0, 1.0, 1.0, -1.0)
+    glMatrixMode(GL_MODELVIEW)
+
+    glLoadIdentity()
+    glRotatef(glfwGetTime() * 50.0, 0.0, 0.0, 1.0)
+
+    glBegin(GL_TRIANGLES)
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(-0.6, -0.4, 0.0)
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(0.6, -0.4, 0.0)
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(0.0, 0.6, 0.0)
+    glEnd()
+
+    glfwSwapBuffers( window )
+    glfwPollEvents()
+  end
+
+  glfwDestroyWindow( window )
+  glfwTerminate()
+end
