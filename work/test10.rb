a44ba58b210007bc2e837fec6500994d66ea8fbe
Koichiro Eto <koichiro.eto@gmail.com>
2021-12-24 03:39:21 +0100

    Update.

--- work/test10.rb
+++ work/test10.rb
@@ -1,16 +1,20 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
+require 'opengl'
+require 'glfw'
+OpenGL.load_lib()
+GLFW.load_lib()
+include OpenGL
+include GLFW
+
 class SGLApp
-  def setup
-    #window 100,100
-    #window 1000,1000
-    #background 0, 10, 20
-    #window 900, 900
-    sdl_init
+  def initialize
+    #sdl_init
+    glfw_init
   end
 
-  def initialize
+  def sdl_init
     window_w = 640
     window_h = 480
     @shadedCube = true
@@ -53,122 +57,193 @@ class SGLApp
        [-0.5, -0.5,  0.5].pack("D3")]
   end
 
-  def display
-    #p "display"
-    color 100, 90, 80
-    line 0, 0, 1000, 1000
-    rect 200, 200, 800, 800
+  def glfw_init
+    # Press ESC to exit.
+    @key_callback = GLFW::create_callback(:GLFWkeyfun) do |window_handle, key, scancode, action, mods|
+      if key == GLFW_KEY_ESCAPE && action == GLFW_PRESS
+        glfwSetWindowShouldClose(window_handle, 1)
+      end
+    end
+
+    glfwInit()
+    @window = glfwCreateWindow( 640, 480, "Simple example", nil, nil )
+    glfwMakeContextCurrent( @window )
+    glfwSetKeyCallback( @window, @key_callback )
+  end
+
+  def setup
+    #window 100,100
+    #window 1000,1000
+    #background 0, 10, 20
+    #window 900, 900
   end
 
   def main(argv)
+    mainloop
+  end
+
+  def mainloop
     loop do
-      while event = SDL2::Event.poll
-        case event
-        when SDL2::Event::Quit, SDL2::Event::KeyDown
-          exit
-        end
+      ret = display
+      if ret
+        glfwDestroyWindow( @window )
+        glfwTerminate()
+        exit
       end
-      glClearColor(0.0, 0.0, 0.0, 1.0);
-      glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-      glBegin(GL_QUADS)
-      color = @color
-      cube = @cube
-      if @shadedCube then
-        glColor3dv(color[0])
-        glVertex3dv(cube[0])
-        glColor3dv(color[1])
-        glVertex3dv(cube[1])
-        glColor3dv(color[2])
-        glVertex3dv(cube[2])
-        glColor3dv(color[3])
-        glVertex3dv(cube[3])
-
-        glColor3dv(color[3])
-        glVertex3dv(cube[3])
-        glColor3dv(color[4])
-        glVertex3dv(cube[4])
-        glColor3dv(color[7])
-        glVertex3dv(cube[7])
-        glColor3dv(color[2])
-        glVertex3dv(cube[2])
-
-        glColor3dv(color[0])
-        glVertex3dv(cube[0])
-        glColor3dv(color[5])
-        glVertex3dv(cube[5])
-        glColor3dv(color[6])
-        glVertex3dv(cube[6])
-        glColor3dv(color[1])
-        glVertex3dv(cube[1])
-
-        glColor3dv(color[5])
-        glVertex3dv(cube[5])
-        glColor3dv(color[4])
-        glVertex3dv(cube[4])
-        glColor3dv(color[7])
-        glVertex3dv(cube[7])
-        glColor3dv(color[6])
-        glVertex3dv(cube[6])
-
-        glColor3dv(color[5])
-        glVertex3dv(cube[5])
-        glColor3dv(color[0])
-        glVertex3dv(cube[0])
-        glColor3dv(color[3])
-        glVertex3dv(cube[3])
-        glColor3dv(color[4])
-        glVertex3dv(cube[4])
-
-        glColor3dv(color[6])
-        glVertex3dv(cube[6])
-        glColor3dv(color[1])
-        glVertex3dv(cube[1])
-        glColor3dv(color[2])
-        glVertex3dv(cube[2])
-        glColor3dv(color[7])
-        glVertex3dv(cube[7])
-      else
-        glColor3d(1.0, 0.0, 0.0)
-        glVertex3dv(cube[0])
-        glVertex3dv(cube[1])
-        glVertex3dv(cube[2])
-        glVertex3dv(cube[3])
-
-        glColor3d(0.0, 1.0, 0.0)
-        glVertex3dv(cube[3])
-        glVertex3dv(cube[4])
-        glVertex3dv(cube[7])
-        glVertex3dv(cube[2])
-
-        glColor3d(0.0, 0.0, 1.0)
-        glVertex3dv(cube[0])
-        glVertex3dv(cube[5])
-        glVertex3dv(cube[6])
-        glVertex3dv(cube[1])
-
-        glColor3d(0.0, 1.0, 1.0)
-        glVertex3dv(cube[5])
-        glVertex3dv(cube[4])
-        glVertex3dv(cube[7])
-        glVertex3dv(cube[6])
-
-        glColor3d(1.0, 1.0, 0.0)
-        glVertex3dv(cube[5])
-        glVertex3dv(cube[0])
-        glVertex3dv(cube[3])
-        glVertex3dv(cube[4])
-
-        glColor3d(1.0, 0.0, 1.0)
-        glVertex3dv(cube[6])
-        glVertex3dv(cube[1])
-        glVertex3dv(cube[2])
-        glVertex3dv(cube[7])
-      end
-      glEnd()
-      glMatrixMode(GL_MODELVIEW)
-      glRotated(5.0, 1.0, 1.0, 1.0)
-      @window.gl_swap
     end
+  end
+
+  def display
+    #p "display"
+    #color 100, 90, 80
+    #line 0, 0, 1000, 1000
+    #rect 200, 200, 800, 800
+    sdl_display
+  end
+
+  def sdl_display
+    endprocess = false
+    while event = SDL2::Event.poll
+      case event
+      when SDL2::Event::Quit, SDL2::Event::KeyDown
+        endprocess = true
+      end
+    end
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_QUADS)
+    color = @color
+    cube = @cube
+    if @shadedCube then
+      glColor3dv(color[0])
+      glVertex3dv(cube[0])
+      glColor3dv(color[1])
+      glVertex3dv(cube[1])
+      glColor3dv(color[2])
+      glVertex3dv(cube[2])
+      glColor3dv(color[3])
+      glVertex3dv(cube[3])
+
+      glColor3dv(color[3])
+      glVertex3dv(cube[3])
+      glColor3dv(color[4])
+      glVertex3dv(cube[4])
+      glColor3dv(color[7])
+      glVertex3dv(cube[7])
+      glColor3dv(color[2])
+      glVertex3dv(cube[2])
+
+      glColor3dv(color[0])
+      glVertex3dv(cube[0])
+      glColor3dv(color[5])
+      glVertex3dv(cube[5])
+      glColor3dv(color[6])
+      glVertex3dv(cube[6])
+      glColor3dv(color[1])
+      glVertex3dv(cube[1])
+
+      glColor3dv(color[5])
+      glVertex3dv(cube[5])
+      glColor3dv(color[4])
+      glVertex3dv(cube[4])
+      glColor3dv(color[7])
+      glVertex3dv(cube[7])
+      glColor3dv(color[6])
+      glVertex3dv(cube[6])
+
+      glColor3dv(color[5])
+      glVertex3dv(cube[5])
+      glColor3dv(color[0])
+      glVertex3dv(cube[0])
+      glColor3dv(color[3])
+      glVertex3dv(cube[3])
+      glColor3dv(color[4])
+      glVertex3dv(cube[4])
+
+      glColor3dv(color[6])
+      glVertex3dv(cube[6])
+      glColor3dv(color[1])
+      glVertex3dv(cube[1])
+      glColor3dv(color[2])
+      glVertex3dv(cube[2])
+      glColor3dv(color[7])
+      glVertex3dv(cube[7])
+    else
+      glColor3d(1.0, 0.0, 0.0)
+      glVertex3dv(cube[0])
+      glVertex3dv(cube[1])
+      glVertex3dv(cube[2])
+      glVertex3dv(cube[3])
+
+      glColor3d(0.0, 1.0, 0.0)
+      glVertex3dv(cube[3])
+      glVertex3dv(cube[4])
+      glVertex3dv(cube[7])
+      glVertex3dv(cube[2])
+
+      glColor3d(0.0, 0.0, 1.0)
+      glVertex3dv(cube[0])
+      glVertex3dv(cube[5])
+      glVertex3dv(cube[6])
+      glVertex3dv(cube[1])
+
+      glColor3d(0.0, 1.0, 1.0)
+      glVertex3dv(cube[5])
+      glVertex3dv(cube[4])
+      glVertex3dv(cube[7])
+      glVertex3dv(cube[6])
+
+      glColor3d(1.0, 1.0, 0.0)
+      glVertex3dv(cube[5])
+      glVertex3dv(cube[0])
+      glVertex3dv(cube[3])
+      glVertex3dv(cube[4])
+
+      glColor3d(1.0, 0.0, 1.0)
+      glVertex3dv(cube[6])
+      glVertex3dv(cube[1])
+      glVertex3dv(cube[2])
+      glVertex3dv(cube[7])
+    end
+    glEnd()
+    glMatrixMode(GL_MODELVIEW)
+    glRotated(5.0, 1.0, 1.0, 1.0)
+    @window.gl_swap
+    return endprocess
+  end
+
+  def glfw_display
+    if glfwWindowShouldClose( @window ) == 0
+      return true
+    end
+    width_ptr = ' ' * 8
+    height_ptr = ' ' * 8
+    glfwGetFramebufferSize(window, width_ptr, height_ptr)
+    width = width_ptr.unpack('L')[0]
+    height = height_ptr.unpack('L')[0]
+    ratio = width.to_f / height.to_f
+
+    glViewport(0, 0, width, height)
+    glClear(GL_COLOR_BUFFER_BIT)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-ratio, ratio, -1.0, 1.0, 1.0, -1.0)
+    glMatrixMode(GL_MODELVIEW)
+
+    glLoadIdentity()
+    glRotatef(glfwGetTime() * 50.0, 0.0, 0.0, 1.0)
+
+    glBegin(GL_TRIANGLES)
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(-0.6, -0.4, 0.0)
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(0.6, -0.4, 0.0)
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(0.0, 0.6, 0.0)
+    glEnd()
+
+    glfwSwapBuffers( window )
+    glfwPollEvents()
   end
 end
 
