bc7ade4ac6b497723e8aff52dcebe4dddc6cb17b
Koichiro Eto <koichiro.eto@gmail.com>
2021-12-24 17:25:50 +0100

    Update.

--- work/test15.rb
+++ work/test15.rb
@@ -112,6 +112,7 @@ class SDLEngine < OpenGLEngine
   end
 
   def display
+    init_viewport
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glBegin(GL_QUADS)
